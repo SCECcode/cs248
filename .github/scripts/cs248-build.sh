a6628a4db01f2af5921673dbf13359edf0c2730b
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2025-05-13 05:59:28 +0000

    fix configure call

--- .github/scripts/cs248-build.sh
+++ .github/scripts/cs248-build.sh
@@ -11,7 +11,7 @@ fi
 aclocal -I m4
 autoconf
 automake --add-missing --force-missing
-./configure --prefix=$UCVM_INSTALL_PATH/model/cs248
+./configure --prefix=$UCVM_INSTALL_PATH/model/cs248 --with-etree-libdir=${UCVM_INSTALL_PATH}/lib/euclid3/lib --with-etree-incdir=${UCVM_INSTALL_PATH}/lib/euclid3/include --with-proj-libdir=${UCVM_INSTALL_PATH}/lib/proj/lib --with-proj-incdir=${UCVM_INSTALL_PATH}/lib/proj/include --with-tiff-libdir=${UCVM_INSTALL_PATH}/lib/tiff/lib --with-tiff-incdir=${UCVM_INSTALL_PATH}/lib/tiff/include --with-sqlite-libdir=${UCVM_INSTALL_PATH}/lib/sqlite/lib --with-sqlite-incdir=${UCVM_INSTALL_PATH}/lib/sqlite/include
 make
 make install
 
